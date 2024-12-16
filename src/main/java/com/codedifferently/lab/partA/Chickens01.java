0615e4ea48f177b58105bde3cad6af51c6f3548c
Brownie124 <121064336+Brownie124@users.noreply.github.com>
2024-12-16 16:42:37 -0500

    Update Chickens01.java
    chickens01

--- src/main/java/com/codedifferently/lab/partA/Chickens01.java
+++ src/main/java/com/codedifferently/lab/partA/Chickens01.java
@@ -1,7 +1,23 @@
 package com.codedifferently.lab.partA;
 
-public class Chickens01 {
-    //Put your code here
 
-    //System.out.println(totalEggs);
+public class Chickens01 {
+    public static void main(String[] args) {
+        //Put your code here
+        int eggsPerChicken = 5;
+        int chickenCount = 3;
+
+        int mondayeggs = eggsPerChicken * chickenCount;
+
+        chickenCount++;
+        int tuesdayeggs = eggsPerChicken * chickenCount;
+
+        chickenCount /= 2;
+        int wednesdayeggs = eggsPerChicken * chickenCount;
+
+        int totalEggs = mondayeggs + tuesdayeggs + wednesdayeggs;
+
+        System.out.println("Total eggs collected: " + totalEggs);
+    }
 }
+
