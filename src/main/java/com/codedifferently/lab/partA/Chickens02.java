00ae06f81f26fb9a60cd358f7994685050d0936f
Brownie124 <121064336+Brownie124@users.noreply.github.com>
2024-12-19 13:28:32 -0500

    Update Chickens02.java
    chickons 02 is done

--- src/main/java/com/codedifferently/lab/partA/Chickens02.java
+++ src/main/java/com/codedifferently/lab/partA/Chickens02.java
@@ -3,9 +3,20 @@ package com.codedifferently.lab.partA;
 public class Chickens02 {
     public static void main(String[] args) {
         //Put your code here
+int mondayeggs = 100;
+int tuesdayeggs = 121;
+int wednesdayeggs = 117;
 
-        //System.out.println("Daily Average:   " +dailyAverage);
-        //System.out.println("Monthly Average: " +monthlyAverage);
-        //System.out.println("Monthly Profit:  $" +monthlyProfit);
+
+int dailyAverage = mondayeggs + tuesdayeggs + wednesdayeggs;
+
+int monthlyAverage = mondayeggs + tuesdayeggs + wednesdayeggs;
+
+double eggprice = 0.18;
+double monthlyProfit = monthlyAverage * eggprice;
+
+        System.out.println("Daily Average:   " +dailyAverage);
+        System.out.println("Monthly Average: " +monthlyAverage);
+        System.out.println("Monthly Profit:  $" +monthlyProfit);
     }
 }
